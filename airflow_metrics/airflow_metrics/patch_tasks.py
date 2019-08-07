5fd6c3f9d928f25c912a8dacb30bb2391ac2d751
hatched-DavidMichon <david.michon@hatchedlabs.com>
2019-08-07 17:29:03 -0300

    add task execution metrics

--- airflow_metrics/airflow_metrics/patch_tasks.py
+++ airflow_metrics/airflow_metrics/patch_tasks.py
@@ -26,6 +26,17 @@ def task_duration(target=None, **kwargs):
         Stats.timing('task.duration', target.duration * 1000, tags=tags)
 
 
+def task_execution(target=None, **kwargs):
+    if target.execution_date and target.start_date:
+        duration = (target.start_date - target.execution_date).total_seconds()
+        tags = {
+            'dag': target.dag_id,
+            'task': target.task_id,
+            'state': target.state,
+            'operator': target.operator,
+        }
+        Stats.timing('task.execution', duration * 1000, tags=tags)
+
 @once
 def patch_tasks():
     dag_run_after_update_manager = EventManager(DagRun, 'after_update')
@@ -33,3 +44,6 @@ def patch_tasks():
 
     task_instance_after_update_manager = EventManager(TaskInstance, 'after_update')
     task_instance_after_update_manager.register_callback('duration', task_duration)
+    
+    task_execution_after_update_manager = EventManager(TaskInstance, 'after_update')
+    task_execution_after_update_manager.register_callback('start_date', task_execution)
